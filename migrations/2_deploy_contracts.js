ff3667f3465a1b57eb6b5e4c98487284c028bf2c
pau1m <paul@freedomthroughweb.com>
2017-09-20 11:35:52 +0100

    Removing boilerplate deployment

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,8 +1,5 @@
-var ConvertLib = artifacts.require("./ConvertLib.sol");
-var MetaCoin = artifacts.require("./MetaCoin.sol");
+let DundeeCoin = artifacts.require("./contracts/token/DundeeCoin.sol");
 
 module.exports = function(deployer) {
-  // deployer.deploy(ConvertLib);
-  // deployer.link(ConvertLib, MetaCoin);
-  // deployer.deploy(MetaCoin);
+  deployer.deploy(DundeeCoin)
 };
